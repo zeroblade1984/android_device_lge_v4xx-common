7d110db3b60943f2ba72ad6da98cca3598804e0d
Sultanxda <sultanxda@gmail.com>
2015-05-23 15:43:54 -0700

    v4xx: Use in-house audio HAL
    *Vanilla CAF

--- v4xx.mk
+++ v4xx.mk
@@ -224,5 +224,6 @@ PRODUCT_COPY_FILES += \
     device/lge/v4xx-common/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 # CAF HALs
+$(call project-set-path,qcom-audio,device/lge/v4xx-common/audio)
 $(call project-set-path,qcom-display,device/lge/v4xx-common/display)
 $(call project-set-path,qcom-media,device/lge/v4xx-common/media)
