b213ff5a1c1e7e8ec2cd1fe6a7417d6d60b04e43
Sultanxda <sultanxda@gmail.com>
2015-05-23 22:11:13 -0700

    v4xx: Update perf options
    *Disabled qc perf daemon (we don't need it)
    *Always keep at least two CPUs online

--- system_prop.mk
+++ system_prop.mk
@@ -62,7 +62,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true \
     ro.qualcomm.cabl=0 \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+    ro.qualcomm.perf.cores_online=2
 
 # Recovery
 PRODUCT_PROPERTY_OVERRIDES += \
